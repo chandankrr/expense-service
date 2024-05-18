41eb513247b1546b386e13ef73fd6294b23f7da8
Chandan Kumar <chandankrr.91@gmail.com>
2024-05-18 19:20:12 +0530

    Create deserializer for expense consumer

---
+++ src/main/java/com/chandankrr/expenseservice/deserializer/ExpenseDeserializer.java
@@ -0,0 +1,30 @@
+package com.chandankrr.expenseservice.deserializer;
+
+import com.chandankrr.expenseservice.dto.ExpenseDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ExpenseDeserializer implements Deserializer<ExpenseDto> {
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {}
+
+    @Override
+    public ExpenseDto deserialize(String topic, byte[] data) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try{
+            return objectMapper.readValue(data, ExpenseDto.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to deserialize ExpenseDto", e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() {}
+}
