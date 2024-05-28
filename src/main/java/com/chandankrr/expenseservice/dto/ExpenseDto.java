0e133e387f6cb57f9460ddc130b0a241b8b87782
Chandan Kumar <chandankrr.91@gmail.com>
2024-05-28 15:44:32 +0530

    Add a string constructor to deserialize the json received from ds service producer

--- src/main/java/com/chandankrr/expenseservice/dto/ExpenseDto.java
+++ src/main/java/com/chandankrr/expenseservice/dto/ExpenseDto.java
@@ -1,10 +1,14 @@
 package com.chandankrr.expenseservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 
 @AllArgsConstructor
@@ -33,5 +37,21 @@ public class ExpenseDto {
 
     @JsonIgnoreProperties(value = "created_at")
     private Timestamp createdAt;
+
+    public ExpenseDto(String json) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+            ExpenseDto expense = mapper.readValue(json, ExpenseDto.class);
+            this.externalId = expense.getExternalId();
+            this.amount = expense.getAmount();
+            this.userId = expense.getUserId();
+            this.merchant = expense.getMerchant();
+            this.currency = expense.getCurrency();
+            this.createdAt = expense.getCreatedAt();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to deserialize ExpenseDto from json", e);
+        }
+    }
 
 }
